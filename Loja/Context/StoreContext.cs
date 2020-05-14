3beaadbe5bd62d9b31ec3961f61022a5accc64a6
Ricardo Almeida <Ricardo.Neiva.Almeida@formandos.cinel.pt>
2020-05-14 01:13:51 +0100

    MVC project

---
+++ Loja/Context/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Loja.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Loja.Context
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
